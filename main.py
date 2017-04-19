f27365fc739707263f7bb1cd45ae2cc42b099152
Mohamed Abdi <mkabdi27@gmail.com>
2017-04-18 21:22:30 -0500

    made changes

--- main.py
+++ main.py
@@ -14,10 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import os
-import webapp2
-import jinja2
-
+import webapp2, cgi, jinja2, os, re
 from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
@@ -25,9 +22,42 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                 autoescape = True)
 
 class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class Body(db.Model):
+    title = db.StringProperty(required = True)
+    body = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class MainPage(Handler):
+    def render_front(self, title="", body="", error=""):
+        self.render("front.html", title = title, body = body, error = error)
+
     def get(self):
-        self.response.write('blog!')
+        self.render_front()
+
+    def post(self):
+        title = self.request.get("title")
+        body = self.request.get("body")
+
+        if title and body:
+            b = Body(title = title, body = body)
+            b.put()
+
+            self.redirect("/blog")
+        else:
+            error = "We need both a title and a body!"
+            self.render_front(title, body, error)
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/blog', MainPage)
 ], debug=True)
